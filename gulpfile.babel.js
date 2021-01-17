cff9a88b0deab8ab2122802da61955324eafaca8
pecalee <studiopeca.lee@gmail.com>
2021-01-17 22:19:47 +0900

    autoprefixer

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
 import sass from "gulp-sass";
+import autop from "gulp-autoprefixer";
 
 sass.compiler = require('node-sass');
 
@@ -49,6 +50,7 @@ const img = () =>
 
 const styles = () => gulp.src(routes.scss.src)
     .pipe(sass().on('error', sass.logError))
+    .pipe(autop())
     .pipe(gulp.dest(routes.scss.dest))
 
 const prepare = gulp.series([clean, img]);
